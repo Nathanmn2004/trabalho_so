64723727a32ca543c9fab342725760b5c4b026cf
Thierry <thierrytorquato@gmail.com>
2024-08-30 11:06:38 -0300

    fcfs changed

--- fcfs.py
+++ fcfs.py
@@ -1,38 +1,39 @@
 def fcfs(lista_de_listas):
     
+    # Ordena a lista de processos com base no tempo de chegada
     lista_ordenada = sorted(lista_de_listas, key=lambda x: x[0])
 
-    #VALOR TEMPO DE RESPOSTA
+    # Inicializa listas para armazenar os tempos de resposta e retorno
     respostas = []
     resp_retorno = []
-    i=0
-    soma =0
-    while 1:
+    
+    # Inicializa variáveis de controle
+    soma = lista_ordenada[0][0]  # Inicia a soma no tempo de chegada do primeiro processo
+    i = 0
+    
+    while i < len(lista_ordenada):
         
-        if i == len(lista_ordenada):
-            break
+        # Ajusta 'soma' caso o próximo processo chegue depois do término do processo atual
+        if soma < lista_ordenada[i][0]:
+            soma = lista_ordenada[i][0]
 
+        # Calcula o tempo de resposta
         respostas.append(soma - lista_ordenada[i][0])
+
+        # Calcula o tempo de retorno
         resp_retorno.append(soma - lista_ordenada[i][0] + lista_ordenada[i][1])
+
+        # Atualiza a soma com a duração do processo atual
         soma += lista_ordenada[i][1]
-
-        if respostas[0] != 0:
-            respostas[0] = 0
-        i+=1
+        
+        i += 1
   
+    # Calcula os tempos médios
     nelem = len(respostas)
-    sumresp = 0
-    for valor in respostas:
-        sumresp += valor
-    trespmedio = sumresp/nelem
-    del respostas
+    
+    trespmedio = sum(respostas) / nelem
+    tretorno = sum(resp_retorno) / nelem
+    tespera = trespmedio
 
-    sumresp = 0
-    for valor in resp_retorno:
-        sumresp += valor
-    tretorno = sumresp/nelem
-    del resp_retorno
-
-    tespera= trespmedio
-
+    # Exibe os resultados
     print(f"FCFS {tretorno:.1f}, {trespmedio:.1f}, {tespera:.1f}")
