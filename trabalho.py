686bf8ee43db924b401cb920675c9bb4cd1a3eba
Thierry <thierrytorquato@gmail.com>
2024-08-30 13:32:35 -0300

    .

--- trabalho.py
+++ trabalho.py
@@ -1,26 +1,21 @@
 from fcfs import fcfs
 from sjc import sjc
+from rr import round_robin
 
 chegadas = []
 tamanhos = []
-    
+
 with open('dados.txt', 'r') as arquivo:
     for linha in arquivo:
-        linha = linha.strip()  # Remove espaços em branco e quebras de linha
-        if linha == "0 0":
-            break
-        numero1, numero2 = linha.split()
-        chegadas.append(float(numero1))
-        tamanhos.append(float(numero2))
-    
+        linha = linha.strip()
 
+        tempo_chegada, tempo_duracao = linha.split()
+
+        chegadas.append(float(tempo_chegada))
+        tamanhos.append(float(tempo_duracao))
 
 lista_combinada = [[chegada, tamanho] for chegada, tamanho in zip(chegadas, tamanhos)]
-    
-
-
-
-
 
 fcfs(lista_combinada)
 sjc(lista_combinada)
+round_robin(lista_combinada)
